66cea8747c7e2a3990414695e7776175ca1275d0
katerji <katerji.abedalaziz@gmail.com>
2023-01-10 21:22:45 +0400

    configure app to receive push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,43 @@ import 'package:y/providers/auth_provider.dart';
 import 'package:y/providers/chat_provider.dart';
 import 'package:y/providers/contact_provider.dart';
 import 'package:y/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
 
-void main() => runApp(
+  print("Handling a background message: ${message.messageId}");
+}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  print('User granted permission: ${settings.authorizationStatus}');
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  print(await FirebaseMessaging.instance.getToken());
+  runApp(
       ChangeNotifierProvider(
         create: (context) => AuthProvider(),
         child: Consumer<AuthProvider>(
@@ -26,6 +61,8 @@ void main() => runApp(
         ),
       ),
     );
+
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
